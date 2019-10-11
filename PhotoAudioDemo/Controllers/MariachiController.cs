13376de9284748fe0c9725d4ea45278915b65161
danwventura <danwventura@gmail.com>
2019-10-11 15:15:15 -0500

    3 more photos, adding correct audio to all 6

---
+++ PhotoAudioDemo/Controllers/MariachiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoAudioDemo.Controllers
+{
+    public class MariachiController : Controller
+    {
+        // GET: Mariachi
+        public ActionResult Photo()
+        {
+            return View();
+        }
+    }
+}
